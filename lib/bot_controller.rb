bad92c80949cab6f727786493b8233fe43760eb3
Alexander Vladimirov <alexander.vladimirov@s-telematics.ru>
2021-02-16 18:39:32 +0300

    Add ruby-version, Fix style, Fix stats_parser

--- lib/bot_controller.rb
+++ lib/bot_controller.rb
@@ -1,5 +1,4 @@
 require 'telegram/bot'
-require_relative 'stats_parser.rb'
 
 class BotController
   BOT_TOKEN = ENV['BOT_TOKEN']
@@ -17,16 +16,16 @@ class BotController
           bot.api.send_message(
             chat_id: message.chat.id,
             text: "Здравствуй, #{message.from.username}\n" \
-            "Для того чтобы получить свежую статистику отправь мне свой ник в Xbox Live"
+                  "Для того чтобы получить свежую статистику отправь мне свой ник в Xbox Live"
           )
         else
           begin
-          user = nickname(message.text)
-          parser = StatsParser.new("https://cod.tracker.gg/warzone/profile/xbl/#{user}/overview")
+          user = username(message.text)
+          stats = parse_stats(create_link(user))
 
           bot.api.send_message(
             chat_id: message.chat.id,
-            text: "Статистика пользователя #{message.text}:\n#{parser.get_stats.join("\n")}"
+            text: "Статистика пользователя #{user}:\n#{stats}"
           )
           rescue URI::InvalidURIError
             bot.api.send_message(
@@ -44,7 +43,17 @@ class BotController
     end
   end
 
-  def nickname(nickname)
-    nickname.split.join('%20')
+  private
+
+  def username(user)
+    user.split.join('%20')
+  end
+
+  def create_link(username, platform = 'xbl')
+    "https://cod.tracker.gg/warzone/profile/#{platform}/#{username}/overview"
+  end
+
+  def parse_stats(link)
+    StatsParser.parse_data(link)
   end
 end
--- lib/stats_parser.rb
+++ lib/stats_parser.rb
@@ -2,28 +2,21 @@ require 'open-uri'
 require 'nokogiri'
 
 class StatsParser
-  def initialize(url)
-    @url = url # 'https://cod.tracker.gg/warzone/profile/xbl/Sou1Jkeee/overview'
-    html = open(url)
-
-    doc = Nokogiri::HTML(html)
-    @main = doc.css('.main').first
-    @giant = doc.css('.giant-stats')
-  end
-
-  def get_stats
-    stats = []
-    @giant.css('.numbers').each do |numbers|
-      value = numbers.css('.value').text
-      name = numbers.css('.name').text
-      stats << "#{name}: #{value}"
+  class << self
+    def parse_data(link)
+      # 'https://cod.tracker.gg/warzone/profile/:platform/:nickname/overview'
+      html = open(link)
+      doc = Nokogiri::HTML(html)
+      arr = [doc.css('.main').first, doc.css('.giant-stats')]
+      stats = []
+      arr.map do |i|
+        i.css('.numbers').each do |numbers|
+          value = numbers.css('.value').text
+          name = numbers.css('.name').text
+          stats << "#{name}: #{value}"
+        end
+      end
+      stats.join("\n")
     end
-
-    @main.css('.numbers').each do |numbers|
-      value = numbers.css('.value').text
-      name = numbers.css('.name').text
-      stats << "#{name}: #{value}"
-    end
-    stats
   end
 end
